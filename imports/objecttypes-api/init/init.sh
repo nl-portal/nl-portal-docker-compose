11ca54ddfcdc846b2fade45f1a04388f22478f0c
Anna Vissart <anna.vissart@ritense.com>
2025-10-01 19:47:53 +0200

    feat: fix objecttypes fixtures and script

--- imports/objecttypes-api/init/init.sh
+++ imports/objecttypes-api/init/init.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
-c=0
-while ! curl http://objecttypen-api:8000
+echo ">>>>  NL Portal init script: Objecttypes API <<<<"
+sleep 2
+while true
 do
-    c=$((c+1))
-    if [ "$c" = "12" ]; then
-        break
+    # Checking whether last table is created
+    initiated=$(pg_isready -h $DB_HOST -q && psql postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/postgres -t -A -c "SELECT EXISTS (SELECT table_name FROM information_schema.tables WHERE table_name = 'token_tokenauth');" || "false")
+    if [ "t" = "${initiated}" ]
+        then
+            echo "Database ready."
+            echo "Attempting to create admin user:"
+            sleep 2
+            DJANGO_SUPERUSER_PASSWORD=admin python /app/src/manage.py createsuperuser --username=admin --email=admin@example.com --noinput
+            echo "Loading fixtures:"
+            sleep 2
+            python /app/src/manage.py loaddata configuration objecttypes
+            break
+        else
+            echo "Database is not ready. Retrying in 10 seconds."
+            sleep 10
     fi
-    sleep 10
 done
 
-python /app/src/manage.py loaddata demodata
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.org', 'admin')" | python /app/src/manage.py shell
+echo ">>>> Done <<<<"
