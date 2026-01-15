3d1be0a3076454299f00252168018cbce55100aa
ErwinBooij <erwin.booij@denhaag.nl>
2026-01-15 08:00:11 +0100

    Fixed the start-setup.sql for openproducten 1.5.0

--- imports/openproduct/database/sql/1-start-setup.sql
+++ imports/openproduct/database/sql/1-start-setup.sql
@@ -20,23 +20,43 @@ VALUES(1, 'pbkdf2_sha256$600000$8R5PtLVB2cWBCB3lBoTrd2$zJ4IalwmR5kNrX9yDl4W+M2F9
 INSERT INTO public.authtoken_token (key, created, user_id)
 VALUES ('ac045222c9e7cde8120b48735560f9b920bb58cd', now(), 1);
 
-INSERT INTO public.zgw_consumers_service(id,label, api_type, api_root, client_id, secret, auth_type, header_key, header_value, oas, nlx, user_id, user_representation, oas_file, client_certificate_id, server_certificate_id, uuid, timeout, api_connection_check_path, slug, jwt_valid_for)
-VALUES (1, 'Notification API', 'nrc', 'http://localhost:8002/api/v1/', 'valtimo_client', 'e09b8bc5-5831-4618-ab28-41411304309d', 'zgw', '', '', 'http://localhost:8002/api/v1/schema/openapi.yaml', '', '', '', '', null, null, '415042b2-c740-4532-be88-38ac5f8c9060', 10, '', 'httplocalhost8001catalogiapiv1', 43200 );
+INSERT INTO public.zgw_consumers_service(id,label, api_type, api_root, client_id, secret, auth_type, header_key, header_value, nlx, user_id, user_representation, client_certificate_id, server_certificate_id, uuid, timeout, api_connection_check_path, slug, jwt_valid_for)
+VALUES (1, 'Notification API', 'nrc', 'http://localhost:8002/api/v1/', 'valtimo_client', 'e09b8bc5-5831-4618-ab28-41411304309d', 'zgw', '', '', '', '', '', null, null, '415042b2-c740-4532-be88-38ac5f8c9060', 10, '', 'httplocalhost8001catalogiapiv1', 43200 );
 
 INSERT INTO public.notifications_api_common_notificationsconfig(id, notifications_api_service_id, notification_delivery_max_retries, notification_delivery_retry_backoff, notification_delivery_retry_backoff_max)
 VALUES (2, 1, 5,3, 48);
--- add externe verwijzing config 
-INSERT INTO public.producttypen_externeverwijzingconfig(id, zaaktypen_url, processen_url, verzoektypen_url, documenten_url, taken_url, zaken_url)
-VALUES (1, 'http://localhost:8001/catalogi/api/v1/zaaktypen', 'http://localhost:8001/catalogi/api/v1', 'http://localhost:8001/catalogi/api/v1', 'http://localhost:8001/documenten/api/v1', 'http://localhost:8010/api/v2/objects', 'http://localhost:8001/zaken/api/v1/zaken');
+-- add externe verwijzing config (doesn't exist in 1.5.0)
+--INSERT INTO public.producttypen_externeverwijzingconfig(id, zaaktypen_url, processen_url, verzoektypen_url, documenten_url, taken_url, zaken_url)
+--VALUES (1, 'http://localhost:8001/catalogi/api/v1/zaaktypen', 'http://localhost:8001/catalogi/api/v1', 'http://localhost:8001/catalogi/api/v1', 'http://localhost:8001/documenten/api/v1', 'http://localhost:8010/api/v2/objects', 'http://localhost:8001/zaken/api/v1/zaken');
 
 -- add dmn config
 INSERT INTO public.producttypen_dmnconfig(id, uuid, naam, tabel_endpoint)
 VALUES ( 1, '3bc964a5-108d-430c-b7fb-6797bb17b6fd', 'watkanikregelen-parkeren', 'http://localhost:9000/engine-rest/decision-definition/key');
 
-SELECT pg_catalog.setval('public.zgw_consumers_service_id_seq', 1, true);
+-- Set sequence for zgw_consumers_service
+SELECT pg_catalog.setval(
+               'public.zgw_consumers_service_id_seq',
+               COALESCE((SELECT MAX(id) FROM public.zgw_consumers_service), 0),
+               true
+       );
 
-SELECT pg_catalog.setval('public.notifications_api_common_notificationsconfig_id_seq', 2, true);
+-- Set sequence for notifications_api_common_notificationsconfig
+SELECT pg_catalog.setval(
+               'public.notifications_api_common_notificationsconfig_id_seq',
+               COALESCE((SELECT MAX(id) FROM public.notifications_api_common_notificationsconfig), 0),
+               true
+       );
 
-SELECT pg_catalog.setval('public.producttypen_externeverwijzingconfig_id_seq', 1, true);
+-- Set sequence for producttypen_externeverwijzingconfig
+SELECT pg_catalog.setval(
+               'public.producttypen_externeverwijzingconfig_id_seq',
+               COALESCE((SELECT MAX(id) FROM public.producttypen_externeverwijzingconfig), 0),
+               true
+       );
 
-SELECT pg_catalog.setval('public.producttypen_dmnconfig_id_seq', 1, true);
+-- Set sequence for producttypen_dmnconfig
+SELECT pg_catalog.setval(
+               'public.producttypen_dmnconfig_id_seq',
+               COALESCE((SELECT MAX(id) FROM public.producttypen_dmnconfig), 0),
+               true
+       );
